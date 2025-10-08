305eda72197cd1363cec0af2e73c384467e5741b
Toluwalase Adeniyi <toladeniyi@noreply.acad.learn2earn.ng>
2025-10-08 16:35:21 +0000

    Delete teacher.sh

--- teacher.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-interviewn= grep -H "licen" interviews/* |grep "\"" | cut -fl -d ":" | rev | cut -fl -d "-" | rev
-interview="cat interviews/interview-$interviewn"
-export interviewnum=$interviewn
-echo $interviewnum
-$interview
-echo $MAIN_SUSPECT
