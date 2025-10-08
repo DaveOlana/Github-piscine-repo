f75a6873517fd77251a90075432506b629bb39a7
toladeniyi <bemintdav@gmail.com>
2025-10-08 15:20:53 +0100

    The great choumi

--- myfamily.sh
+++
@@ -1,1 +0,0 @@
-curl -s https://acad.learn2earn.ng/assets/superhero/all.json | \jq --arg id "$HERO_ID" -r ' .[] | select(.id == ($id |tonumber)) | .connections.relatives | @json' | \sed 's/^"\(.*\)"$/\1/'
