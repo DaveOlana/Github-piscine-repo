26007fd42b35956aa674d62d5fdcc7c359d32c14
toladeniyi <bemintdav@gmail.com>
2025-10-08 16:41:23 +0100

    the teacher file

---
+++ the-final-cl-test-master/mystery/teacher.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+interviewn= 'grep -H "licen" interviews/* |grep "\"" | cut -fl -d ":" | rev | cut -fl -d "-" | rev'
+interview="cat interviews/interview-$interviewn"
+export interviewnum=$interviewn
+echo $interviewnum
+$interview
+echo $MAIN_SUSPECT
