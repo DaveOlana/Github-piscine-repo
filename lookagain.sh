19211fd21a5085ab8912b7a3f03af6e26e8fc18d
toladeniyi <bemintdav@gmail.com>
2025-10-08 14:59:09 +0100

    search file on lookagain

--- lookagain.sh
+++ lookagain.sh
@@ -3,7 +3,7 @@
 # Script: looksagain.sh
 # Description: Find all .sh files (recursively), remove the .sh extension, and show names in ascending order
 
-find . -type f -name "*.sh" 
+find . -type f -name "*.sh" \
     | rev | cut -d'/' -f1 | rev \
     | cut -d'.' -f1 \
     | sort -r
