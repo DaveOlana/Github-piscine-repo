560f6f07bbe53a31bb1ff0b6fd6e78444d94a97f
toladeniyi <bemintdav@gmail.com>
2025-10-08 13:14:09 +0100

    Test two for combined test 1

--- myfamily.sh
+++ myfamily.sh
@@ -1,1 +1,2 @@
-curl -s https://acad.learn2earn.ng/assets/superhero/all.json | \jq --arg id "$HERO_ID" -r ' .[] | select(.id == ($id |tonumber)) | .connections.relatives | @json' | \sed 's/^"\(.*\)"$/\1/'
+curl -s https://acad.learn2earn.ng/assets/superhero/all.json \
+  | jq -r --arg id "$HERO_ID" '.[] | select(.id == ($id | tonumber)) | .connections.relatives | gsub("\\n"; "\n")'
