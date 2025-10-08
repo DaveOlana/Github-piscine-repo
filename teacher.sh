8d6e0675cb77599a886298ac713a9cda17358a05
toladeniyi <bemintdav@gmail.com>
2025-10-08 17:34:11 +0100

    The teacher file

--- teacher.sh
+++ teacher.sh
@@ -1,8 +1,8 @@
-#!/bin.bash
+#!/bin/bash
 
 interviewn=`grep -H "licen" interviews/* |grep "\"" | cut -f1 -d ":" | rev | cut -f1 -d "-" | rev`
 interview="cat interviews/interview-$interviewn"
 export interviewnum=$interviewn
 echo $interviewnum
 $interview
-echo $MAIN_SUSPECT   
+echo $MAIN_SUSPECT
