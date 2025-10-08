450ae52f881e04290f39af0db3738ac904f14072
toladeniyi <bemintdav@gmail.com>
2025-10-08 16:32:59 +0100

    the teacher file

--- teacher.sh
+++ teacher.sh
@@ -2,6 +2,7 @@
 
 interviewn= grep -H "licen" interviews/* |grep "\"" | cut -fl -d ":" | rev | cut -fl -d "-" | rev
 interview="cat interviews/interview-$interviewn"
+export interviewnum=$interviewn
 echo $interviewnum
 $interview
 echo $MAIN_SUSPECT
