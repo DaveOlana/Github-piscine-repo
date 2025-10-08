7e92ef936821b29269a99ef738f96811091554c8
toladeniyi <bemintdav@gmail.com>
2025-10-08 16:53:07 +0100

    the teacher file

--- the-final-cl-test-master/mystery/teacher.sh
+++ the-final-cl-test-master/mystery/teacher.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-interviewn= 'grep -H "licen" interviews/* |grep "\"" | cut -fl -d ":" | rev | cut -fl -d "-" | rev'
-interview="cat interviews/interview-$interviewn"
+interviewn=$(grep -H "licen" interviews/* | grep "\"" | cut -f1 -d ":" | rev | cut -f1 -d "-" | rev)
+cat interviews/interview-$interviewn
 export interviewnum=$interviewn
 echo $interviewnum
 $interview
