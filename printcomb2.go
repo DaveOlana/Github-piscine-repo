8f54cb56e9d56259dbd10c717512640d37ff672c
toladeniyi <bemintdav@gmail.com>
2025-10-09 16:22:59 +0100

    The new printcomb2 file

--- printcomb2.go
+++ printcomb2.go
@@ -5,11 +5,11 @@ import "github.com/01-edu/z01"
 func PrintComb2() {
 	for a := 0; a <= 98; a++ {
 		for b := a + 1; b <= 99; b++ {
-			z01.PrintRune(rune(a/10) + '0')
-			z01.PrintRune(rune(a%10) + '0')
+			z01.PrintRune('0' + a/10)
+			z01.PrintRune('0' + a%10)
 			z01.PrintRune(' ')
-			z01.PrintRune(rune(b/10) + '0')
-			z01.PrintRune(rune(b%10) + '0')
+			z01.PrintRune('0' + b/10)
+			z01.PrintRune('0' + b%10)
 			if a != 98 || b != 99 {
 				z01.PrintRune(',')
 				z01.PrintRune(' ')
