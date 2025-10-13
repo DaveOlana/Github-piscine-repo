763fb6b416e76833959b5cb730d9504c53f2b949
toladeniyi <bemintdav@gmail.com>
2025-10-13 15:20:20 +0100

    New trial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,23 +1,13 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	if nb < 0 {
+	if nb < 0 || nb > 20 { // <- prevent overflow or long loops
 		return 0
-	}
-
-	if nb == 0 || nb == 1 {
-		return 1
 	}
 
 	result := 1
-
-	for i := 2; i <= nb; i++ {
+	for i := 1; i <= nb; i++ {
 		result *= i
 	}
-
-	if result < 0 {
-		return 0
-	}
-
 	return result
 }
