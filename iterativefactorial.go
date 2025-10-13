32c01498e5d2d740ac515dafd7e101c5b0213c79
toladeniyi <bemintdav@gmail.com>
2025-10-13 15:08:41 +0100

    The new code

---
+++ iterativefactorial.go
@@ -0,0 +1,14 @@
+package piscine
+
+func IterativeFactorial(nb int) int {
+	if nb < 0 || nb > 10 || nb == 0 {
+		return 0
+	}
+
+	result := 1
+
+	for i := 2; i <= nb; i++ {
+		result *= i
+	}
+	return result
+}
